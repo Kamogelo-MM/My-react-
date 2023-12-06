563ce9fe37f59844cb8361ba8eb6a9e91a111487
Kamogelo-MM <kammolawa@gmail.com>
2023-12-07 00:34:39 +0200

    weather app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <WeatherSearch />
     </div>
   );
 }
